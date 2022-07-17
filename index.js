d291c829c9f89a3a31c78785c85f38d08a42865b
ferdinandmukodo <ferdinant.mukodo@student.moringaschool.com>
2022-07-17 10:50:06 +0300

    It will edit colors and backgrounds of html elements.

--- index.js
+++ index.js
@@ -115,3 +115,13 @@ function resetPage(){
     location.reload();
 
 }
+
+function dateTime(){
+    var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var dateTime = date+' '+time;
+}
